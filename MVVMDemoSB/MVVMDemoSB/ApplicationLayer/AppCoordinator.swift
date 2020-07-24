a683a307279131faad8493019cb01f250b0ba2bb
sherry <sherry.bajwa@idc.bedbath.com>
2020-07-24 17:59:56 +0530

    Added Coordinators (Dependency injectors) to the Application layer and Presentatioin layer

---
+++ MVVMDemoSB/MVVMDemoSB/ApplicationLayer/AppCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  AppCoordinator.swift
+//  MVVMDemoSB
+//
+//  Created by Sherry Bajwa on 21/07/20.
+//  Copyright © 2020 Sherry Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+    let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        if #available(iOS 13.0, *) {
+            navigationController.overrideUserInterfaceStyle = .light
+        }
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        let factsCoordinator = FactsCoordinatorImplementation(navigationController: navigationController)
+        coordinate(to: factsCoordinator)
+        
+    }
+}
+
---
+++ MVVMDemoSB/MVVMDemoSB/CoreLayer/Bases/Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinator.swift
+//  MVVMDemoSB
+//
+//  Created by Sherry Bajwa on 21/07/20.
+//  Copyright © 2020 Sherry Bajwa. All rights reserved.
+//
+
+protocol Coordinator: class {
+    func start()
+    func coordinate(to coordinator: Coordinator)
+}
+
+extension Coordinator {
+    func coordinate(to coordinator: Coordinator) {
+        coordinator.start()
+    }
+}
